94cd984ea7f2d830f8a7047325d8a2cfe16f6ace
Owen Monsma <darkfire613@me.com>
2017-04-23 22:04:07 -0500

    fixed ParInd

--- ParIndMode.java
+++ ParIndMode.java
@@ -97,9 +97,13 @@ class ParIndMode implements RaceMode{
             tempRacer.startStamp = theTimer.timeStamp();
             switch (startedQueue){
                 case TRACK_ONE:
-                    racingQueue1.add(tempRacer);
+                  System.out.println("Adding " + tempRacer.id + " to track1");
+                  racingQueue1.add(tempRacer);
+                  break;
                 case TRACK_TWO:
-                    racingQueue2.add(tempRacer);
+                  System.out.println("Adding " + tempRacer.id + " to track2");
+                  racingQueue2.add(tempRacer);
+                  break;
                 default:
                     break;
             }
@@ -163,23 +167,30 @@ class ParIndMode implements RaceMode{
         Racer r1 = racingQueue1.peek();
         BigDecimal bd = new BigDecimal(theTimer.getTime() - r1.startTime);
         bd = bd.setScale(2, RoundingMode.HALF_UP);
-        s += r1.id + Time.timeConversion(bd.doubleValue()) + " R\n";
+        s += r1.id + "  " + Time.timeConversion(bd.doubleValue()) + " R\n";
       }
 
       if (!racingQueue2.isEmpty()){
         Racer r2 = racingQueue2.peek();
         BigDecimal bd = new BigDecimal(theTimer.getTime() - r2.startTime);
         bd = bd.setScale(2, RoundingMode.HALF_UP);
-        s += r2.id + Time.timeConversion(bd.doubleValue()) + " R\n\n";
+        s += r2.id + "  " + Time.timeConversion(bd.doubleValue()) + " R\n\n";
       }
+
+      s += "\n";
 
       if(!finishedList.isEmpty()){
-        s += finishedList.get(finishedList.size() - 1).id;
-        s += " " + Time.timeConversion(finishedList.get(finishedList.size() - 1).raceTime) + " F\n";
-        s += finishedList.get(finishedList.size() - 2).id;
-        s += " " + Time.timeConversion(finishedList.get(finishedList.size() - 2).raceTime) + " F\n\n";
-      }
+        if (finishedList.size() >= 1){
+          s += finishedList.get(finishedList.size() - 1).id;
+          s += " " + Time.timeConversion(finishedList.get(finishedList.size() - 1).raceTime) + " F\n";
+        }
+        if (finishedList.size() >= 2){
+          s += finishedList.get(finishedList.size() - 2).id;
+          s += " " + Time.timeConversion(finishedList.get(finishedList.size() - 2).raceTime) + " F\n\n";
+        }
 
+      }
+      s += "\n";
       return s;
     }
 
